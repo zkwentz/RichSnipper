c75225a1636e6141245c4b65377a197c644184f3
zkwentz <zkwentz@gmail.com>
2013-07-16 20:27:32 -0700

    iterator created need to find only the root level itemscopes to begin consumption

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -23,39 +23,46 @@ chrome.extension.sendMessage({}, function(response) {
 // object made up of all the schema elements on the page.
 var consumeSchema = function() {
 	var $schema = $('[itemscope]'),
-		result = {};
+		result = new Array();
 
-	if ($schema.length >= 1)
-	{
-		var $context = $schema.first();
-
-		result.id = $context.attr('id');
-		result.itemscope = $context.attr('itemscope');
-		result.properties = new Array();
-		result.properties[0] = {};
-		result.properties[0].propname = $context.attr('itemprop');
-		result.properties[0].propvalue = $context.html();
-
-		var $properties = $context.find('[itemprop]');
-
-		$.each($properties,function(index){
-			var $this = $(this);
-			console.log(index);
-			console.log($this);
-			if (!$this.attr('itemscope'))
-			{
-				result.properties[index+1] = {};
-				result.properties[index+1].propname = $this.attr('itemprop');
-				result.properties[index+1].propvalue = $this.html();
-			}
-			else
-			{
-				//do it again
-			}
-		});
-	}
+	$.each($schema,function(index){
+		console.log(this);
+		var $this = $(this);
+		result[index] = iterator($this);
+	});
 
  	return result;
+}
+
+var iterator = function($scope)
+{
+	var result = {};
+	result.id = $scope.attr('id');
+	result.itemscope = $scope.attr('itemtype');
+	result.properties = new Array();
+	$.each($scope.find('[itemprop]'),function(index){
+		var $this = $(this),
+			item = {};
+
+		if ($this.attr('itemscope'))
+		{
+			result.properties[index] = iterator($this);
+		}
+		else
+		{
+			if (!$this.data('rs-processed'))
+			{
+				item.propname = $this.attr('itemprop');
+				item.propvalue = $this.text();
+
+				result.properties[index] = item;
+
+				$this.data('rs-processed',true);
+			}
+		}
+	});
+
+	return result;
 }
 
 
